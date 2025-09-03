974b111f8cfec415fc4902d3ec783a4b6743e7c1
Ahmad Taiwo <ahmadtaiwo15@gmail.com>
2025-09-03 11:27:56 -1100

    options updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -128,12 +128,12 @@ const Home: React.FC = () => {
   return (
     <div className="max-w-xl mx-auto px-4 sm:px-6 lg:max-w-3xl lg:px-8 py-6 pt-16">
       <OrderInfo
-        slaughterDate="Saturday, 30th Aug"
-        orderDeadline="Thursday, 28th Aug 11:59 PM"
+        slaughterDate="Saturday, 13th Sept"
+        orderDeadline="Thursday, 11th Sept 11:59 PM"
         cowHeadPrice="Ranges between ₦50,000 - ₦70,000"
         slotPrice="₦7,500"
         cowLegs="4 available(FCFS) Price ranges between ₦7,000 - ₦10,000"
-        cowTailPrice="₦30,000 - ₦40,000"
+        cowTailPrice="₦20,000 - ₦30,000"
       />
 
       {/* Form */}
